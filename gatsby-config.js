aedca152596b541ab168dc447f0466135bc77440
hayul7805 <71222422+hayul7805@users.noreply.github.com>
2022-10-08 20:15:06 +0900

    return to default setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,18 +6,7 @@ module.exports = {
         contentPath: `${__dirname}/content/posts`,
         rootNote: `/main`,
       },
-    },
-    `gatsby-plugin-netlify`,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-highlight-code`,
-          },
-        ],
-      },
-    },
+    }
   ],
   siteMetadata: {
     title: `Hayul's Tech Garden`,
